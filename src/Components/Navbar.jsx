66adc5ed2f8d40c46d0a611379da9f92adf4f871
mdaasim77 <khanaasim9910@gmail.com>
2025-11-25 17:46:10 +0530

    new images and code

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,10 +5,59 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 export default function Navbar() {
   return (
     <>
-      <nav className="navbar p-3">
-        <i className="bi bi-list text-white"></i>
+      <nav className="navbar container">
+        <div className="nav-box1 d-flex text-white">
+          <img src="\assets\Menu-btn.svg" alt="" className="p-2 nav-menu" />
+          <div className="search-div">
+            <input
+              type="search"
+              name="search"
+              placeholder="Search..."
+              id="search"
+              className="search-input"
+            />
+            <img
+              src="\assets\search-icon.svg"
+              alt="search-icon"
+              className="nav-search"
+            />
+          </div>
+          <div className="d-flex">
+            <label>Branch Name</label>
+            <div className="dropdown">
+              <button
+                className="dropdown-toggle  dropdownbtn"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Select Branch
+              </button>
+              <ul className="dropdown-menu" id="list">
+                <li>
+                  <a className="dropdown-item">Noida</a>
+                </li>
+                <li>
+                  <a className="dropdown-item">Delhi</a>
+                </li>
+                <li>
+                  <a className="dropdown-item">Ghaziabad</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
 
-        <i className="bi bi-search text-white"></i>
+        <div className="nav-box2 d-flex bg-danger">
+          <img src="\assets\Notification.svg" alt="" />
+          <img src="\assets\nav-profile.svg" alt="" />
+          <div className="profile_card d-flex">
+            <div>
+              <h4>Aasim</h4>
+              <h6>khanaasim9910@gmail.com</h6>
+            </div>
+            <img src="\assets\iconamoon_arrow.svg" alt="" />
+          </div>
+        </div>
       </nav>
     </>
   );
