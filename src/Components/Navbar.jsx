2700db81f54185726fae42f6d02bed8246f9f7ff
mdaasim77 <khanaasim9910@gmail.com>
2025-11-26 18:45:51 +0530

    26-nov Dashboard code start

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,68 +1,102 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import DB_Card from "./DB_Card";
 
 export default function Navbar() {
   return (
     <>
-      <nav className="navbar row">
-        <div className="nav-box1 d-flex text-white  col-lg-6 col-xs-12">
-          <img src="\assets\Menu-btn.svg" alt="" className="p-2 nav-menu" />
-          <div className="search-div">
-            <input
-              type="search"
-              name="search"
-              placeholder="Search..."
-              id="search"
-              className="search-input"
-            />
-            <img
-              src="\assets\search-icon.svg"
-              alt="search-icon"
-              className="nav-search "
-            />
-          </div>
-
-          <div className="d-flex">
-            <label>Branch Name</label>
-            <div className="dropdown">
-              <button
-                className="dropdown-toggle  dropdownbtn"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Select Branch
-              </button>
-              <ul className="dropdown-menu" id="list">
-                <li>
-                  <a className="dropdown-item">Noida</a>
-                </li>
-                <li>
-                  <a className="dropdown-item">Delhi</a>
-                </li>
-                <li>
-                  <a className="dropdown-item">Ghaziabad</a>
-                </li>
-              </ul>
-            </div>
-          </div>
+      <div className="row ">
+        <div className="col-2 border border-danger">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam
+          assumenda consequuntur ratione commodi, consequatur facere saepe enim
+          magnam repellat ipsa?
         </div>
+        <div className="col-10 border border-success ">
+          {/* ................................................ */}
+          {/* ................................................ */}
 
-        <div
-          className="nav-box2 d-flex justify-content-end col-lg-6 col-xs-12 justify-content-sm-center justify-content-lg-end
+          {/* navbar content start from here */}
+
+          {/* ................................................ */}
+          {/* ................................................ */}
+
+          <nav className="navbar row">
+            <div className="nav-box1 d-flex text-white  col-lg-8 col-xs-10  justify-content-evenly">
+              <img src="\assets\Menu-btn.svg" alt="" className="p-2 nav-menu" />
+              <div className="search-div p-1  border border-white rounded-2 d-flex align-items-center ">
+                <input
+                  type="search"
+                  name="search"
+                  placeholder="Search..."
+                  id="search"
+                  className=" search-input border-0 border-end"
+                />
+                <img
+                  src="\assets\search-icon.svg"
+                  alt="search-icon"
+                  className="nav-search "
+                />
+              </div>
+
+              <div className="d-flex">
+                <label>Branch Name</label>
+                <div className="dropdown">
+                  <button
+                    className="dropdown-toggle  dropdownbtn"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Select Branch
+                  </button>
+                  <ul className="dropdown-menu" id="list">
+                    <li>
+                      <a className="dropdown-item">Noida</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item">Delhi</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item">Ghaziabad</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div
+              className=" nav-box2 d-flex justify-content-end  align-items-center col-lg-4 col-xs-2 justify-content-sm-center justify-content-lg-end
 "
-        >
-          <img src="\assets\Notification.svg" className="me-2" alt="" />
-          <img src="\assets\nav-profile.svg" className="me-2" alt="" />
-          <div className="profile_card d-flex me-2 text-white">
-            <div>
-              <h4>Aasim</h4>
-              <h6>khanaasim9910@gmail.com</h6>
+            >
+              <img src="\assets\Notification.svg" className="me-4" alt="" />
+              <img src="\assets\nav-profile.svg" className="me-2" alt="" />
+              <div className="profile_card d-flex me-2 text-white">
+                <div className="align-item-center">
+                  <h4 className="mb-0 mt-2">Aasim</h4>
+                  <p className="mt-0 email">khanaasim9910@gmail.com</p>
+                </div>
+                <img
+                  src="\assets\iconamoon_arrow.svg"
+                  className="me-2"
+                  alt=""
+                />
+              </div>
             </div>
-            <img src="\assets\iconamoon_arrow.svg" className="me-2" alt="" />
-          </div>
+          </nav>
+
+          {/* ................................................ */}
+          {/* ................................................ */}
+
+          {/* navbar code end */}
+
+          {/* ................................................ */}
+
+          {/* main content first row code */}
+
+          {/* ................................................ */}
+          {/* ................................................ */}
+          <DB_Card />
         </div>
-      </nav>
+      </div>
     </>
   );
 }
