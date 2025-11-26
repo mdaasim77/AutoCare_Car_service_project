f28bd69c2253afe0e5a563fee180f430500b10c8
mdaasim77 <khanaasim9910@gmail.com>
2025-11-26 14:15:24 +0530

    26-nov first half navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,8 +5,8 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 export default function Navbar() {
   return (
     <>
-      <nav className="navbar container">
-        <div className="nav-box1 d-flex text-white">
+      <nav className="navbar row">
+        <div className="nav-box1 d-flex text-white  col-lg-6 col-xs-12">
           <img src="\assets\Menu-btn.svg" alt="" className="p-2 nav-menu" />
           <div className="search-div">
             <input
@@ -19,9 +19,10 @@ export default function Navbar() {
             <img
               src="\assets\search-icon.svg"
               alt="search-icon"
-              className="nav-search"
+              className="nav-search "
             />
           </div>
+
           <div className="d-flex">
             <label>Branch Name</label>
             <div className="dropdown">
@@ -47,15 +48,18 @@ export default function Navbar() {
           </div>
         </div>
 
-        <div className="nav-box2 d-flex bg-danger">
-          <img src="\assets\Notification.svg" alt="" />
-          <img src="\assets\nav-profile.svg" alt="" />
-          <div className="profile_card d-flex">
+        <div
+          className="nav-box2 d-flex justify-content-end col-lg-6 col-xs-12 justify-content-sm-center justify-content-lg-end
+"
+        >
+          <img src="\assets\Notification.svg" className="me-2" alt="" />
+          <img src="\assets\nav-profile.svg" className="me-2" alt="" />
+          <div className="profile_card d-flex me-2 text-white">
             <div>
               <h4>Aasim</h4>
               <h6>khanaasim9910@gmail.com</h6>
             </div>
-            <img src="\assets\iconamoon_arrow.svg" alt="" />
+            <img src="\assets\iconamoon_arrow.svg" className="me-2" alt="" />
           </div>
         </div>
       </nav>
