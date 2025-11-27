53034953d4c91d68d61537f4b62bd7fd801375a8
mdaasim77 <khanaasim9910@gmail.com>
2025-11-27 13:11:19 +0530

    Dashboard card layout ready 27-nov first half

--- src/Components/DB_Card.jsx
+++ src/Components/DB_Card.jsx
@@ -3,65 +3,106 @@ import React from "react";
 export default function DB_Card() {
   return (
     <>
-      <div className="row">
-        <div className="col-3 border border-primary">
-          <div className="d-flex">
-            <div>
-              <h5>Job Card</h5>
-              <p>20</p>
+      <div className="row g-4 m-3">
+        <div className="container col-12 col-md-6 col-xl-3 ">
+          <div className=" border card_1 p-3 rounded-4 ">
+            <div className="d-flex justify-content-between ">
+              <div className="d-flex flex-column align-items-start mt-2">
+                <h5 className="fs-6">Today's Job Card</h5>
+                <p className="mt-1 fs-4 mb-0">20</p>
+              </div>
+              <div className="my-3 mx-5">
+                <div className="imageedit">
+                  <img src="\assets\card_1.svg" alt="" />
+                </div>
+              </div>
             </div>
-            <div>
-              <img src="\assets\Menu-btn.svg" alt="" />
+            <hr className="border border-secondary"></hr>
+            <div className="d-flex gap jsutify-content-around">
+              <p className=" firstroecard bg-white  px-2 mx-auto">3 Intake</p>
+              <p className=" firstroecard bg-white  px-2 mx-auto">
+                5 In-progress
+              </p>
+              <p className=" firstroecard bg-white  px-2 mx-auto">
+                4 Delivered
+              </p>
             </div>
-          </div>
-          <hr className="border border-danger"></hr>
-          <div className="row align-items-center">
-            <h6 className="p-2 firstroecard border border-danger rounded-4 text-center col-3">
-              3 Intake
-            </h6>
-            <h6 className="p-2 firstroecard border border-danger rounded-4 text-center col-5">
-              5 In-progress
-            </h6>
-            <h6 className="p-2 firstroecard border border-danger rounded-4 text-center col-4">
-              4 Delivered
-            </h6>
           </div>
         </div>
-        <div className="col-3 border border-primary">
-          <div className="d-flex">
-            <div>
-              <h5>Job Card</h5>
-              <p>20</p>
+        <div className="container col-12 col-md-6 col-xl-3 ">
+          <div className="border card_1 p-3 rounded-4">
+            <div className="d-flex justify-content-between ">
+              <div className="d-flex flex-column align-items-start mt-2">
+                <h5 className="fs-6">Today's Job Card</h5>
+                <p className="mt-1 fs-4 mb-0">20</p>
+              </div>
+              <div className="my-3 mx-5">
+                <div className="imageedit">
+                  <img src="\assets\card_1.svg" alt="" />
+                </div>
+              </div>
             </div>
-            <div>
-              <img src="\assets\Menu-btn.svg" alt="" />
+            <hr className="border border-secondary"></hr>
+            <div className="d-flex gap jsutify-content-around">
+              <p className=" firstroecard bg-white  px-2 mx-auto">3 Intake</p>
+              <p className=" firstroecard bg-white  px-2 mx-auto">
+                5 In-progress
+              </p>
+              <p className=" firstroecard bg-white  px-2 mx-auto">
+                4 Delivered
+              </p>
             </div>
           </div>
-          <hr className="border border-danger"></hr>
         </div>
-        <div className="col-3 border border-primary">
-          <div className="d-flex">
-            <div>
-              <h5>Job Card</h5>
-              <p>20</p>
+        <div className="container col-md-6 col-xl-3 col-12">
+          <div className=" border card_1 p-3 rounded-4">
+            <div className="d-flex justify-content-between ">
+              <div className="d-flex flex-column align-items-start mt-2">
+                <h5 className="fs-6">Today's Job Card</h5>
+                <p className="mt-1 fs-4 mb-0">20</p>
+              </div>
+              <div className="my-3 mx-5">
+                <div className="imageedit">
+                  <img src="\assets\card_1.svg" alt="" />
+                </div>
+              </div>
             </div>
-            <div>
-              <img src="\assets\Menu-btn.svg" alt="" />
+            <hr className="border border-secondary"></hr>
+            <div className="d-flex gap justify-content-around">
+              <p className=" firstroecard bg-white  px-2 mx-auto">3 Intake</p>
+              <p className=" firstroecard bg-white  px-2 mx-auto">
+                5 In-progress
+              </p>
+              <p className=" firstroecard bg-white  px-2 mx-auto">
+                4 Delivered
+              </p>
             </div>
           </div>
-          <hr className="border border-danger"></hr>
         </div>
-        <div className="col-3 border border-primary">
-          <div className="d-flex">
-            <div>
-              <h5>Job Card</h5>
-              <p>20</p>
+        <div className="container col-md-6 col-xl-3 col-12">
+          <div className=" border card_1 p-3 rounded-4">
+            <div className="d-flex justify-content-between ">
+              <div className="d-flex flex-column align-items-start mt-2">
+                <h5 className="fs-6">Today's Job Card</h5>
+                <p className="mt-1 fs-4 mb-0">20</p>
+              </div>
+              <div className="my-3 mx-5">
+                <div className="imageedit">
+                  <img src="\assets\card_1.svg" alt="" />
+                </div>
+              </div>
             </div>
-            <div>
-              <img src="\assets\Menu-btn.svg" alt="" />
+            <hr className="border border-secondary"></hr>
+            <div className="d-flex gap jsutify-content-around">
+              <p className=" firstroecard bg-white  px-2 mx-auto">3 Intake</p>
+              <p className=" firstroecard bg-white  px-2 mx-auto">
+                5 In-progress
+              </p>
+              <p className=" firstroecard bg-white  px-2 mx-auto">
+                4 Delivered
+              </p>
             </div>
           </div>
-          <hr className="border border-danger"></hr>
         </div>
       </div>
     </>
