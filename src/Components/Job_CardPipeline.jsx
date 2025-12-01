cb3b88662f3ecf3a41ba2bb7c74433b1ea65f1de
mdaasim77 <khanaasim9910@gmail.com>
2025-12-01 13:29:33 +0530

    box 2 design complete 1 half 01-dec

--- src/Components/Job_CardPipeline.jsx
+++ src/Components/Job_CardPipeline.jsx
@@ -74,44 +74,44 @@ export default function Job_CardPipeline() {
             <div className="border purpleclr rounded-4 ms-4">
               {/*card code start */}
               <div className="card m-4">
-                <div className="d-flex">
-                  <div className="pipelinecardptag border m-3">
+                <div className="d-flex mt-3">
+                  <div className="pipelinecardptag border m-0">
                     <h6>Intake</h6>
                   </div>
 
                   <div
                     className="horizontalline border rounded-3 border-dark h-25
-                    mt-5 "
+                    mt-3 "
                   ></div>
 
-                  <div className="pipelinecardptag border m-3">
+                  <div className="pipelinecardptag border m-0">
                     <h6>Estimate</h6>
                   </div>
 
                   <div
                     className="horizontalline border rounded-3 border-dark h-25
-                    mt-5 "
+                    mt-3 "
                   ></div>
 
-                  <div className="pipelinecardptag border m-3">
+                  <div className="pipelinecardptag border m-0">
                     <h6>Waiting Approval</h6>
                   </div>
 
                   <div
                     className="horizontalline border rounded-3 border-dark h-25
-                    mt-5 "
+                    mt-3 "
                   ></div>
 
-                  <div className="pipelinecardptag border m-3">
+                  <div className="pipelinecardptag border m-0">
                     <h6>Billing</h6>
                   </div>
 
                   <div
                     className="horizontalline border rounded-3 border-dark h-25
-                    mt-5 "
+                    mt-3 "
                   ></div>
 
-                  <div className="pipelinecardptag border m-3">
+                  <div className="pipelinecardptag border m-0">
                     <h6>Delivered</h6>
                   </div>
                 </div>
@@ -136,7 +136,96 @@ export default function Job_CardPipeline() {
           </div>
         </div>
         {/* job pipeline box 1 code end*/}
-        <div className="container-fluid col-lg-6 col-sm-12">2</div>
+
+        {/* =============================================== */}
+        {/* box 2 code start here */}
+        {/* =============================================== */}
+
+        <div className="container-fluid col-lg-6 col-sm-12">
+          <div>
+            <div className="d-flex justify-content-between ">
+              <h2 className="todayjob">Today's Job</h2>
+              <button className="viewallbtn me-4">View All</button>
+            </div>
+            <div className="row ">
+              <div className="col-xl-6 col-sm-12">
+                <div id="box1cng" className="m-1 border rounded-1 me-1">
+                  <div className="w-50 mx-5 mt-3">
+                    <h2 className="inp">In-Progress</h2>
+                  </div>
+                  <div className="d-flex justify-content-center">
+                    <img src="assets/profile.svg" className="mx-2" alt="" />
+                    <div>
+                      <h4>Aasim Gaur</h4>
+                      <p className="box2p">Today's, 11:00 AM</p>
+                      <p className="box2p">Vehical No. UP 12DC 4388</p>
+                    </div>
+                  </div>
+                  <div className="container p-2 text-center">
+                    <button className="viewbtn px-3">View</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-6 col-sm-12  ">
+                <div id="box2cng" className="m-1 border rounded-1 me-4">
+                  <div className="w-50 mx-5 mt-3">
+                    <h2 className="inp">In</h2>
+                  </div>
+                  <div className="d-flex justify-content-center">
+                    <img src="assets/profile.svg" className="mx-2" alt="" />
+                    <div>
+                      <h4>Aasim Gaur</h4>
+                      <p className="box2p">Today's, 11:00 AM</p>
+                      <p className="box2p">Vehical No. UP 12DC 4388</p>
+                    </div>
+                  </div>
+
+                  <div className="container p-2 text-center">
+                    <button className="viewbtn px-3">View</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xl-6 col-sm-12 ">
+                <div id="box3cng" className="m-1 border rounded-1 me-1  ">
+                  <div className="w-50 mx-5 mt-3">
+                    <h2 className="inp">In-Progress</h2>
+                  </div>
+                  <div className="d-flex justify-content-center">
+                    <img src="assets/profile.svg" className="mx-2" alt="" />
+                    <div>
+                      <h4>Aasim Gaur</h4>
+                      <p className="box2p">Today's, 11:00 AM</p>
+                      <p className="box2p">Vehical No. UP 12DC 4388</p>
+                    </div>
+                  </div>
+                  <div className="container p-2 text-center">
+                    <button className="viewbtn px-3">View</button>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-6 col-sm-12  ">
+                <div id="box4cng" className="m-1 border rounded-1 me-4">
+                  <div className="w-50 mx-5 mt-3">
+                    <h2 className="inp">In-Progress</h2>
+                  </div>
+                  <div className="d-flex justify-content-center">
+                    <img src="assets/profile.svg" className="mx-2" alt="" />
+                    <div>
+                      <h4>Aasim Gaur</h4>
+                      <p className="box2p">Today's, 11:00 AM</p>
+                      <p className="box2p">Vehical No. UP 12DC 4388</p>
+                    </div>
+                  </div>
+                  <div className="container p-2 text-center">
+                    <button className="viewbtn px-3">View</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
