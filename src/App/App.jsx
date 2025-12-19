e4074b48ee06260760f569d99798e3a1249f25d9
mdaasim77 <khanaasim9910@gmail.com>
2025-12-19 11:28:25 +0530

    add new layout in project

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import DB_Card from "./DB_Card";
-import Job_CardPipeline from "./Job_CardPipeline";
-import UpComingSrv from "./UpComingSrv";
-import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
+import DB_Card from "../Pages/DB_Card";
+import Job_CardPipeline from "../Pages/Job_CardPipeline";
+import UpComingSrv from "../Pages/UpComingSrv";
+import Sidebar from "../Components/Sidebar";
+import Navbar from "../Components/Navbar";
 import { Route, Routes } from "react-router-dom";
-import Customer from "./Customer";
+import Customer from "../Pages/Customer";
 
 export default function App() {
   const [responsiveSideBar, setResponsiveSideBar] = useState(false);
