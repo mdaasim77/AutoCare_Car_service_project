07fa2b3574ae8af9ff8304cb1f909a547c488a74
mdaasim77 <khanaasim9910@gmail.com>
2025-12-19 10:57:02 +0530

    ok

--- src/Components/Customer.jsx
+++ src/Components/Customer.jsx
@@ -1,3 +1,19 @@
+import React from "react";
+
 export default function Customer() {
-  return <div></div>;
+  return (
+    <>
+      <div className="container">
+        <div className="border border-primary row ">
+          <div className="col-md-6 d-flex flex-column border ">
+            <div>
+              <h2 className="fs-6">Home Customers</h2>
+            </div>
+            <div>2</div>
+          </div>
+          <div className="col-md-6">btn</div>
+        </div>
+      </div>
+    </>
+  );
 }
