7e08f0cf9609f35166692358faf912e6a56daa7a
mdaasim77 <khanaasim9910@gmail.com>
2025-12-22 20:20:34 +0530

    top work complete

--- src/Pages/Customer.jsx
+++ src/Pages/Customer.jsx
@@ -5,14 +5,20 @@ export default function Customer() {
     <>
       <div className="customerpage">
         <div>
-          <h6>Home {">"} Customers</h6>
+          <h6 className="hc">
+            Home {">"} <b>Customers</b>
+          </h6>
         </div>
-        <div className="bg-danger d-flex flex-wrape justify-content-between ">
-          <h6>Customer Details</h6>
-          <div>
-            <button>Export</button>
-            <button>Filter</button>
-            <button>+ Add Customer</button>
+        <div className=" row">
+          <div className=" col-md-6 col-sm-6">
+            <h6 className="mt-3">Customer Details</h6>
+          </div>
+          <div className="d-flex justify-content-end p-2 flex-wrape col-md-6 col-sm-6">
+            <button className="me-2" id="exportbtn">
+              Export
+            </button>
+            <button className="me-2 filter">Filter</button>
+            <button className="me-2 filter">+ Add Customer</button>
           </div>
         </div>
       </div>
